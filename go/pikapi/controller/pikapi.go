4ca127d2f654a5fdf9c01e1149573fb3161a087c
niuhuan <niuhuancn@outlook.com>
2021-08-11 14:21:56 +0800

    misstake commit hotfix

--- go/pikapi/controller/pikapi.go
+++ go/pikapi/controller/pikapi.go
@@ -379,7 +379,23 @@ func loadView(comicId string) (string, error) {
 		return string(b), nil
 	}
 	return "", nil
-}que
+}
+
+var commands = map[string]string{
+	"windows": "cmd /c start",
+	"darwin":  "open",
+	"linux":   "xdg-open",
+}
+
+// Open calls the OS default program for uri
+func open(uri string) error {
+	run, ok := commands[runtime.GOOS]
+	if !ok {
+		return fmt.Errorf("don't know how to open things on %s platform", runtime.GOOS)
+	}
+	cmd := exec.Command(run, uri)
+	return cmd.Start()
+}
 
 func FlatInvoke(method string, params string) (string, error) {
 	switch method {
